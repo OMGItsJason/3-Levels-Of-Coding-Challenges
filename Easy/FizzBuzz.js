5c32583b325902aaded5651d2477121f21abb575
Jason Chanliongco Maliquid <jasonm2822@gmail.com>
2024-01-29 19:26:34 +0800

    FizzBuzz FInish

--- Easy/FizzBuzz.js
+++ Easy/FizzBuzz.js
@@ -3,10 +3,14 @@
 
 function FizzBuzz() {
   for (let i = 1; i <= 100; i++) {
-    if (i % 2 === 0 && i % 3 === 0) console.log("FizzBuzz");
-    else if (i % 3 === 0) console.log("Fizz");
-    else if (i % 5 === 0) console.log("Buzz");
-    else console.log(i);
+    //Used For Loop To 1 To 100
+    if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
+    //Using Modulus Operator To Check If Divisible By 3 And 5. If Divisible Print FizzBuzz
+    else if (i % 3 === 0)
+      console.log("Fizz"); // Else If Divisible By 3 Print Fizz
+    else if (i % 5 === 0)
+      console.log("Buzz"); // Else If Divisible By 5 Print Buzz
+    else console.log(i); // Else The Conditions Are Not Met
   }
 }
 
